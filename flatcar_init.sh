51d736af4b3ba79c402d0d4585a456b60bcde2a6
TheoKeen <48030134+TheoKeen@users.noreply.github.com>
2021-04-30 17:26:27 +0200

    Update flatcar_init.sh

--- flatcar_init.sh
+++ flatcar_init.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 #remove the symbolic link.
-rm ~/.bashrc
+#rm ~/.bashrc
 
-create alias for docker-compose
-echo alias docker-compose="'"'docker run --rm \
+#create alias for docker-compose
+echo alias docker-compose="'"'/usr/bin/docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v "$PWD:$PWD" \
     -w="$PWD" \
     docker/compose:1.29.0'"'" >> ~/.bashrc
     
+shopt -s expand_aliases    
 source ~/.bashrc
 
 cd ~
