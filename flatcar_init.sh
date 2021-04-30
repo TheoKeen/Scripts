fceca4e41da320a33f206a3656a80a0bdbbf5840
TheoKeen <48030134+TheoKeen@users.noreply.github.com>
2021-04-30 16:38:02 +0200

    Update flatcar_init.sh

--- flatcar_init.sh
+++ flatcar_init.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+#remove the symbolic link.
+rm ~/.bashrc
 
+create alias for docker-compose
+echo alias docker-compose="'"'docker run --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v "$PWD:$PWD" \
+    -w="$PWD" \
+    docker/compose:1.29.0'"'" >> ~/.bashrc
+    
+source ~/.bashrc
+
+cd ~
+git clone https://github.com/TheoKeen/docker-compose
+cd ~/docker-compose
+docker-compose up -d
 
 mkdir /opt/keennews
