47baf1cf7f3bede88b440dbc79e0a865b058f5e5
TheoKeen <48030134+TheoKeen@users.noreply.github.com>
2023-01-19 16:06:09 +0100

    Update InstallProxmox.sh

--- InstallProxmox.sh
+++ InstallProxmox.sh
@@ -4,4 +4,4 @@
 hostnamectl set-hostname pve-tink01 --static
 sed -i '3 i 10.240.0.20     pve-tink01' /etc/hosts
 
-apt install proxmox-ve postfix open-iscsi- -y
+apt install proxmox-ve postfix open-iscsi -y
