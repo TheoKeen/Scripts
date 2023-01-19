01a0bab6492916a45c81fc654b00f3a82df3f2af
TheoKeen <48030134+TheoKeen@users.noreply.github.com>
2023-01-19 16:55:27 +0100

    Update InstallProxmox.sh

--- InstallProxmox.sh
+++ InstallProxmox.sh
@@ -2,6 +2,20 @@
 
 
 hostnamectl set-hostname pve-tink01 --static
-sed -i '3 i 10.240.0.20     pve-tink01' /etc/hosts
+sed -i '3 i 172.18.4.200 pve-tink01' /etc/hosts
+
+echo pve-tink01 > /etc/hostname
+
+mkdir /run/network
+
+cat << EOF > /etc/network/interfaces
+auto lo
+iface lo inet loopback
+
+auto enp2s0
+iface enp2s0 inet dhcp
+
+EOF
+
 
 apt install proxmox-ve postfix open-iscsi -y
