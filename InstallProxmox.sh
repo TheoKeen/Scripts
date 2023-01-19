1391e29d3c1cb32263452c2d933fb00d18a8f2ed
TheoKeen <48030134+TheoKeen@users.noreply.github.com>
2023-01-19 16:57:42 +0100

    Update InstallProxmox.sh

--- InstallProxmox.sh
+++ InstallProxmox.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+mv /etc/resolv.conf /etc/resolv.conf.bak
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
 
-hostnamectl set-hostname pve-tink01 --static
+#hostnamectl set-hostname pve-tink01 --static
 sed -i '3 i 172.18.4.200 pve-tink01' /etc/hosts
 
 echo pve-tink01 > /etc/hostname
@@ -19,3 +21,5 @@ EOF
 
 
 apt install proxmox-ve postfix open-iscsi -y
+
+mv /etc/resolv.conf.bak /etc/resolv.conf
