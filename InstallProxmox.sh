881a5dc28d94a852739bbf4d798f80aa8d3a7b00
TheoKeen <48030134+TheoKeen@users.noreply.github.com>
2023-01-31 11:44:13 +0100

    Update InstallProxmox.sh

--- InstallProxmox.sh
+++ InstallProxmox.sh
@@ -4,10 +4,8 @@ apt-get install jq -y
 
 Thostname=$(curl -s $TinkIP:50061/metadata | jq -r .metadata.instance.hostname)
 Tip=$(curl -s $TinkIP:50061/metadata | jq  -r .network.interfaces[].dhcp.ip.address)
+Tgateway=$(curl -s $TinkIP:50061/metadata | jq  -r .network.interfaces[].dhcp.ip.gateway)
 
-echo ${Thostname} > /root/hostname
-
-#hostnamectl set-hostname pve-tink01 --static
 sed -i "3 i ${Tip} ${Thostname}" /etc/hosts
 
 echo ${Thostname} > /etc/hostname
@@ -19,11 +17,16 @@ dpkg-reconfigure openssh-server
 apt install proxmox-ve -y
 
 cat << EOF > /etc/network/interfaces.new
-auto lo
-iface lo inet loopback
-
-auto enp2s0
-iface enp2s0 inet dhcp
+auto vmbr0
+iface vmbr0 inet static
+	address ${Tip}/24
+	gateway ${Tgateway}
+	bridge-ports enp2s0
+	bridge-stp off
+	bridge-fd 0
+        bridge-vids 2-20
+        bridge_vlan_aware yes
+        bridge-access 4
 
 EOF
 
