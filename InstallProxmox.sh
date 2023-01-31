17c2858864f9fdd1f2999f82aeabc0b737dae0f5
TheoKeen <48030134+TheoKeen@users.noreply.github.com>
2023-01-31 09:36:59 +0100

    Update InstallProxmox.sh

--- InstallProxmox.sh
+++ InstallProxmox.sh
@@ -3,6 +3,8 @@
 Thostname=$(curl -s $TinkIP:50061/metadata | jq -r .metadata.instance.hostname)
 Tip=$(curl -s $TinkIP:50061/metadata | jq  -r .network.interfaces[].dhcp.ip.address)
 
+echo ${Thostname} > /root/hostname
+
 #hostnamectl set-hostname pve-tink01 --static
 sed -i "3 i ${Tip} ${Thostname}" /etc/hosts
 
