53cc66bf4165f7ac06afe29a35008ef2cf9681f2
TheoKeen <48030134+TheoKeen@users.noreply.github.com>
2023-02-01 11:33:57 +0100

    Update PostInstallProxmox.sh

--- PostInstallProxmox.sh
+++ PostInstallProxmox.sh
@@ -4,6 +4,8 @@ if [ ! -d /var/log/Tink ]; then
   mkdir /var/log/Tink
 fi
 
+echo "frank ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
 touch /var/log/Tink/PostInstallProxmox.sh
 
 /usr/sbin/lvcreate -l 90%FREE -T cloud/data
