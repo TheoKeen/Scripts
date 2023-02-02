d8e957f549a7e449105e5a3786734b9e501bbf18
TheoKeen <48030134+TheoKeen@users.noreply.github.com>
2023-02-02 18:56:33 +0100

    Update PostInstallProxmox.sh

--- PostInstallProxmox.sh
+++ PostInstallProxmox.sh
@@ -8,5 +8,10 @@ echo "frank ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
 touch /var/log/Tink/PostInstallProxmox.sh
 
-/usr/sbin/lvcreate -l 90%FREE -T cloud/data
-/usr/sbin/pvesm add lvmthin local-lvm --vgname cloud --thinpool data
+if [  -b /dev/nvme0n1p2 ]; then
+zpool create -o ashift=13 -o autotrim=on pvedata /dev/nvme0n1p2
+pvesm add zfspool --blocksize 64k pvedata -pool pvedata
+fi
+
+#/usr/sbin/lvcreate -l 90%FREE -T cloud/data
+#/usr/sbin/pvesm add lvmthin local-lvm --vgname cloud --thinpool data
